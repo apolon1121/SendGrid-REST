2d2ae29f0e1bbdb68bdc6ef4bbe690d9137d35b3
Apolon Pachulia <a.pachulia@hotmail.com>
2015-02-19 18:00:17 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require_once 'class/class.sendemail.php';
 
 $Email=new sendEmail();
 
-$Email::sendNewEmail("a.pachulia@hotmail.com","Test Subject","Test Email Body");
+$Email::sendGrid("a.pachulia@hotmail.com","Test Subject","Test HTML Email Body","Test Plain Text Body");
